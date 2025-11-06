efe79a18a962cf73af59a0c8486ee4dc6a42f4d1
ilhams26 <ilhamsimarmata.26@gmail.com>
2025-11-06 21:12:57 +0700

    set up

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const btnLogout = document.getElementById("btnLogout");
-    const modal = document.getElementById("logoutModal");
-    const confirmBtn = document.getElementById("confirmLogout");
-    const cancelBtn = document.getElementById("cancelLogout");
+  const btnLogout = document.getElementById("btnLogout");
+  const modal = document.getElementById("logoutModal");
+  const confirmBtn = document.getElementById("confirmLogout");
+  const cancelBtn = document.getElementById("cancelLogout");
 
-    btnLogout.addEventListener("click", () => modal.classList.add("active"));
-    cancelBtn.addEventListener("click", () => modal.classList.remove("active"));
-    confirmBtn.addEventListener("click", () => {
-        window.location.href = "logout.php";
-    });
+  btnLogout.addEventListener("click", () => modal.classList.add("active"));
+  cancelBtn.addEventListener("click", () => modal.classList.remove("active"));
+  confirmBtn.addEventListener("click", () => {
+    window.location.href = "../logout.php";
+  });
 });
